308771a07d660520bcc85e389eeb54519f37a86a
changjie <doupi.cj@raycloud.com>
2020-06-04 21:16:15 +0800

    feat: 使用nacos作为规则数据源

--- dubbo-sentinel-provider/src/main/java/com/dubbo/example/provider/DubboProviderBootstrap.java
+++ dubbo-sentinel-provider/src/main/java/com/dubbo/example/provider/DubboProviderBootstrap.java
@@ -1,8 +1,12 @@
 package com.dubbo.example.provider;
 
+import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
+import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -20,9 +24,32 @@ import java.util.List;
 @SpringBootApplication
 public class DubboProviderBootstrap {
 
+    // nacos server ip 默认端口8848
+    private static final String remoteAddress = "localhost";
+    // nacos group
+    private static final String groupId = "Sentinel:Demo";
+    // nacos dataId
+    private static final String dataId = "com.alibaba.csp.sentinel.demo.flow.rule";
+
+    /**
+     * nacosJSON格式的配置
+     * [
+     *     {
+     *         "resource": "com.dubbo.example.DemoService:sayHello(java.lang.String)",
+     *         "limitApp": "default",
+     *         "grade": 1,
+     *         "count": 2,
+     *         "strategy": 0,
+     *         "controlBehavior": 0,
+     *         "clusterMode": false
+     *     }
+     * ]
+     * @param args
+     */
     public static void main(String[] args) {
 
 //        initFlowRules();
+        loadRules();
 
         new SpringApplicationBuilder(DubboProviderBootstrap.class)
                 .run(args);
@@ -40,5 +67,12 @@ public class DubboProviderBootstrap {
         flowRule.setLimitApp("sentinel-web");//流控针对的调用来源，若为 default 则不区分调用来源
         rules.add(flowRule);
         FlowRuleManager.loadRules(rules);
+    }
+
+    private static void loadRules() {
+        ReadableDataSource<String, List<FlowRule>> flowRuleDataSource = new NacosDataSource<>(remoteAddress, groupId, dataId,
+                source -> JSON.parseObject(source, new TypeReference<List<FlowRule>>() {
+                }));
+        FlowRuleManager.register2Property(flowRuleDataSource.getProperty());
     }
 }
